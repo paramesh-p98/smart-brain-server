ee4abd176d292f6fac687b665dc90ac58a8074cf
paramesh-p98 <paramesh3598@gmail.com>
2021-11-21 00:53:23 +0530

    updating database info

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image');
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-curved-72591',
         port: 5432,
         user: 'postgres',
         password: 'paramesh98',
