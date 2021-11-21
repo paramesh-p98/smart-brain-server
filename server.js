acc7dab05a16ef4779095dd3d0998a4f4ef1f532
paramesh-p98 <paramesh3598@gmail.com>
2021-11-21 18:45:25 +0530

    "checking git"

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));  //body-parser deprecated in l
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send("Server Working") })
+app.get('/', (req, res) => { res.send("Server not Working") })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, knex, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, knex) })
